2d837207583ddf8776407ec72bbe631f7086df5f
infosys <infosys@C4222>
2025-04-21 09:57:54 +0900

    プロジェクト ファイルを追加します。

---
+++ chapter01/ProductSample/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ProductSample {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Product karint = new Product(123, "かりんとう", 180);
+            Product okasi = new Product(111, "おかし", 120);
+
+            //税抜きの価格表示
+            Console.WriteLine(karint.Name + "の税抜き価格は" + karint.Price + "円です");
+            //消費税額の表示
+            Console.WriteLine(karint.Name + "の消費税額は" + karint.GetTax() + "円です");
+            //税込み価格の表示
+            Console.WriteLine(karint.Name + "の税込み価格は" + karint.GetPriceIncludingTax() +"円です");
+
+            Console.WriteLine(okasi.Name + "の税抜き価格は" + okasi.Price + "円です");
+            Console.WriteLine(okasi.Name + "の消費税額は" + okasi.GetTax() + "円です");
+            Console.WriteLine(okasi.Name + "の税込み価格は" + okasi.GetPriceIncludingTax() + "円です");
+
+
+        }
+    }
+}
---
+++ chapter02/DistanceConverter/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace DistanceConverter {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int st = int.Parse(args[1]);
+            int en = int.Parse(args[2]);
+
+            if (args.Length >= 1 && args[0] == "-tom") {
+                PrintFeetToMeterList(st, en);
+            } else {
+                PrintMeterToFeetList(st, en);
+            }
+        }
+        //フィートからメートルへの対応表を出力
+        static void PrintFeetToMeterList(int st, int en) {
+            for (int feet = st; feet <= en; feet++) {
+                double meter = FeetToMeter(feet);
+                Console.WriteLine($"{feet}ft = {meter:0.0000}m");
+            }
+
+        }
+        //メートルからフィートへの対応表を出力
+        static void PrintMeterToFeetList(int st, int en) {
+
+            for (int meter = st; meter <= en; meter++) {
+                double feet = MeterToFeet(meter);
+                Console.WriteLine($"{meter}m = {feet:0.0000}ft");
+            }
+        }
+
+        static double FeetToMeter(int feet) {
+            return feet * 0.3408;
+        }
+        static double MeterToFeet(int meter) {
+            return meter / 0.3408;
+
+
+        }
+    }
+}
