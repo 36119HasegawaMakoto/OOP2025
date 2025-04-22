7b9cb43b0c5344c1f69928ba30e6474dc09cdb69
36119hasegawaMakoto <oibc36119@ojs.ac.jp>
2025-04-22 10:25:28 +0900

    インスタンスメゾットとして利用

--- chapter02/DistanceConverter/FeetConverter.cs
+++ chapter02/DistanceConverter/FeetConverter.cs
@@ -7,11 +7,11 @@ using System.Threading.Tasks;
 namespace DistanceConverter {
     public class FeetConverter {
         //フィートからメートルへの対応表を出力
-        public static double FromMeter(double meter) {
+        public double FromMeter(double meter) {
             return meter / 0.3048;
         }
         //メートルからフィートへの対応表を出力
-        public static double ToMeter(double feet) {
+        public double ToMeter(double feet) {
             return feet * 0.3048;
         }
     }
--- chapter02/DistanceConverter/Program.cs
+++ chapter02/DistanceConverter/Program.cs
@@ -14,28 +14,20 @@
         }
         //フィートからメートルへの対応表を出力
         static void PrintFeetToMeterList(int st, int en) {
+            FeetConverter fc = new FeetConverter();
             for (int feet = st; feet <= en; feet++) {
-                double meter = FeetToMeter(feet);
+                double meter = fc.FromMeter(feet);
                 Console.WriteLine($"{feet}ft = {meter:0.0000}m");
             }
 
         }
         //メートルからフィートへの対応表を出力
         static void PrintMeterToFeetList(int st, int en) {
-
+            FeetConverter fc = new FeetConverter();
             for (int meter = st; meter <= en; meter++) {
-                double feet = MeterToFeet(meter);
+                double feet = fc.ToMeter(meter);
                 Console.WriteLine($"{meter}m = {feet:0.0000}ft");
             }
-        }
-
-        static double FeetToMeter(int feet) {
-            return feet * 0.3408;
-        }
-        static double MeterToFeet(int meter) {
-            return meter / 0.3408;
-
-
         }
     }
 }
