0dba69ffa181bcca8333b6fe1ade5bccba7e465a
36119hasegawaMakoto <oibc36119@ojs.ac.jp>
2025-04-22 11:36:50 +0900

    インチからメートル変換完成

---
+++ chapter02/演習問題/Exercise02/InchConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    public static class InchConverter {
+
+        //定数
+        private const double retio = 0.0254;
+
+
+        public static double FromMeter(double meter) {
+            return meter * retio;
+        }
+
+    }
+}
---
+++ chapter02/演習問題/Exercise02/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            //１～１０
+            PrintInchToMeterList(1, 10);
+
+            //表の出力
+            static void PrintInchToMeterList(int st, int en) {
+                for (int inch = st; inch <= en; inch++) {
+                    double meter = InchConverter.FromMeter(inch);
+                    Console.WriteLine($"{inch}inch = {meter:0.0000}m");
+
+                }
+            }
+        }
+    }
+}
