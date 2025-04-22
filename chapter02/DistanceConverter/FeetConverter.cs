87bd8ef5599fcb115f3d1cc263787d33bb8b4b2a
36119hasegawaMakoto <oibc36119@ojs.ac.jp>
2025-04-22 09:56:20 +0900

    ファイル追加

--- chapter02/DistanceConverter/FeetConverter.cs
+++ chapter02/DistanceConverter/FeetConverter.cs
@@ -10,9 +10,6 @@ namespace DistanceConverter {
         public static double FromMeter(double meter) {
             return meter / 0.3048;
         }
-
-
-
         //メートルからフィートへの対応表を出力
         public static double ToMeter(double feet) {
             return feet * 0.3048;
