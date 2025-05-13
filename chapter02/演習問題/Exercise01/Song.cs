7f151ff032d80bb0cf3b8805add91c6df08da65c
36119hasegawaMakoto <oibc36119@ojs.ac.jp>
2025-05-13 10:03:08 +0900

    2.2.1 完成

---
+++ chapter02/演習問題/Exercise01/Song.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    public class Song {
+        //2.1.1
+        string Title;
+        string AristName;
+        int Length;
+    }
+}
