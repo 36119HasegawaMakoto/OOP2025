580fde7aa0a0e43cbebc67d3e9cf5cc5f8b900d2
36119hasegawaMakoto <oibc36119@ojs.ac.jp>
2025-05-23 13:50:13 +0900

    ①と②完成

--- chapter04/演習問題/Exercise01/Program.cs
+++ chapter04/演習問題/Exercise01/Program.cs
@@ -15,11 +15,33 @@ namespace Exercise01 {
         }
 
         private static void Exercise1(List<string> langs) {
-
+            //foreach文
+            foreach (var item in langs) {
+                if (item.Contains("S"))
+                    Console.WriteLine(item);
+            }
+            
+            //for文
+            for (int i = 0; i < langs.Count; i++) {
+                if (langs[i].Contains("S")) {
+                    Console.WriteLine(langs[i]);
+                }
+            }
+            //where文
+            int hw = 0;
+            while (hw < langs.Count) {
+                if (langs[hw].Contains("S")) {
+                    Console.WriteLine(langs[hw]);
+                }
+                hw++;
+            }
         }
 
         private static void Exercise2(List<string> langs) {
-
+            var foreac = langs.Where(s => s.Contains('S'));
+            foreach (var item in foreac) {
+                Console.WriteLine(item);
+            }
         }
 
         private static void Exercise3(List<string> langs) {
