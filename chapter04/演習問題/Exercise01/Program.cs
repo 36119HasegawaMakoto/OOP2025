554d40e81f48184f3cdbc5a6090e5c5dd8fff9b9
36119hasegawaMakoto <oibc36119@ojs.ac.jp>
2025-05-23 14:44:39 +0900

    ③完成

--- chapter04/演習問題/Exercise01/Program.cs
+++ chapter04/演習問題/Exercise01/Program.cs
@@ -45,7 +45,9 @@ namespace Exercise01 {
         }
 
         private static void Exercise3(List<string> langs) {
-
+            var lang = langs.Find(s => s.Length == 10) ?? "unknown";
+            Console.WriteLine(lang);
+            
         }
     }
 }
