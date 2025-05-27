490adcf170529377e8a0696abc1de4ceb13dc173
36119hasegawaMakoto <oibc36119@ojs.ac.jp>
2025-05-27 09:26:10 +0900

    Chapter04 完成

--- chapter04/演習問題/Exercise02/Exercise02/Program.cs
+++ chapter04/演習問題/Exercise02/Exercise02/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+
 namespace Exercise02 {
     internal class Program {
         static void Main(string[] args) {
@@ -10,10 +12,10 @@ namespace Exercise02 {
         }
 
         private static void Exercise1() {
-            Console.Write("一番の値入力してね：");
-            string num = Console.ReadLine();
+            Console.Write("1番の値入力してね：");
+            var num = Console.ReadLine();
             if (int.TryParse(num, out var number)) {
-                if (number <= -1) {
+                if (number < 0) {
                     Console.WriteLine(number);
                 }
                 if (number >= 0 && number < 100) {
@@ -28,14 +30,46 @@ namespace Exercise02 {
             } else {
                 Console.WriteLine("入力値に誤りがあります");
             }
-            }
+        }
 
         private static void Exercise2() {
-
+            Console.Write("2番の値入力してね：");
+            var num = Console.ReadLine();
+            if (int.TryParse(num, out var number)) {
+                switch (number) {
+                    case < 0:
+                        Console.WriteLine(number);
+                        break;
+                    case < 100:
+                        Console.WriteLine(number * 2);
+                        break;
+                    case < 500:
+                        Console.WriteLine(number * 3);
+                        break;
+                    default:
+                        Console.WriteLine(number);
+                        break;
+                }
+            } else {
+                Console.WriteLine("入力値に誤りがあります");
+            }
         }
 
         private static void Exercise3() {
-
+            Console.Write("3番の値入力してね：");
+            var line = Console.ReadLine();
+            if (int.TryParse(line, out var number)) {
+                var text = number switch {
+                    < 0 => number,
+                    < 100 => number * 2,
+                    < 500 => number * 3,
+                    _ => number
+                };
+                Console.WriteLine(text);
+            } else {
+                Console.WriteLine("入力値に誤りがあります");
+            }
         }
     }
 }
+
