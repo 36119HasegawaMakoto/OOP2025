7ec519a109787ec446a51710923377a8dc118e79
36119hasegawaMakoto <oibc36119@ojs.ac.jp>
2025-05-28 11:10:01 +0900

    1~3完成

---
+++ LINQ/LingSample/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace LingSample {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var numbers = Enumerable.Range(1, 100);
+
+            //合計値を出力
+            Console.WriteLine(numbers.Sum());
+            //平均
+            Console.WriteLine(numbers.Average());
+            //偶数合計
+            Console.WriteLine(numbers.Where(n => n % 2 ==0).Sum());
+            //偶数のでっかい値
+            Console.WriteLine(numbers.Where(n => n % 2 == 0).Max());
+            //8の倍数の合計
+            Console.WriteLine(numbers.Where(n => n % 8 == 0).Sum());
+
+
+
+            //foreach (var num in numbers) {
+            //    Console.WriteLine(num);
+            //}
+
+
+
+        }
+    }
+}
---
+++ chapter07/Section01/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var books = Books.GetBooks();
+
+            //本の平均金額を表示
+            Console.WriteLine((int)books.Average(x => x.Price));
+            //本のページ合計表示
+            Console.WriteLine((int)books.Sum(x => x.Pages));
+            //一番金額の安い書籍名と金額を表示
+            var no3 = books.Where(x => x.Price == books.Min(b=>b.Price));
+            foreach(var item in no3) {
+                Console.WriteLine(item.Title + ":" + item.Price);
+            }
+            //一番ページが多い書籍名とページを表示
+            var no4 = books.Where(x => x.Pages == books.Max(b => b.Pages));
+            foreach (var item in no4) {
+                Console.WriteLine(item.Title + ":" + item.Pages + "ページ");
+            }
+            //タイトルに「物語が」が含まれている書籍名をすべて表示
+            var no5 = books.Where(s => s.Title.Contains("物語"));
+            foreach (var item in no5) {
+                Console.WriteLine(item.Title);
+            }
+        }
+    }
+}
