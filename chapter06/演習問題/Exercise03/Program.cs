39ab808c954dcf4d0b16ad56902c3d792678b2bf
36119hasegawaMakoto <oibc36119@ojs.ac.jp>
2025-05-30 15:30:27 +0900

    3番以外完成

---
+++ chapter06/演習問題/Exercise03/Program.cs
@@ -0,0 +1,50 @@
+﻿
+namespace Exercise03 {
+    internal class Program {
+        static void Main(string[] args) {
+            var text = "Jackdaws love my big sphinx of quartz";
+
+            Console.WriteLine("6.3.1");
+            Exercise1(text);
+
+            Console.WriteLine("6.3.2");
+            Exercise2(text);
+
+            Console.WriteLine("6.3.3");
+            Exercise3(text);
+
+            Console.WriteLine("6.3.4");
+            Exercise4(text);
+
+            Console.WriteLine("6.3.5");
+            Exercise5(text);
+
+        }
+
+        private static void Exercise1(string text) {
+            Console.WriteLine("空白数:" + text.Count(char.IsWhiteSpace));            
+        }
+
+        private static void Exercise2(string text) {
+            Console.WriteLine(text.Replace("big", "small"));
+        }
+
+        private static void Exercise3(string text) {
+
+        }
+
+        private static void Exercise4(string text) {
+            string[] word = text.Split(" ");
+            Console.WriteLine("単語数:" + word.Length);
+        }
+
+        private static void Exercise5(string text) {
+            string[] word = text.Split(" ");
+            for (int i = 0; word.Length > i; i++) {
+                if (word[i].Length <= 4) {
+                    Console.WriteLine(word[i]);
+                }
+            }
+        }
+    }
+}
