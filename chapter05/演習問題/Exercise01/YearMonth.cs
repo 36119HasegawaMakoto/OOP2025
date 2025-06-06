7e5476880ce05ca97250ccc50d3a855482e01c0c
36119hasegawaMakoto <oibc36119@ojs.ac.jp>
2025-06-06 15:10:17 +0900

    5.1.1, 5.1.2完成

---
+++ chapter05/演習問題/Exercise01/YearMonth.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    //5.1.1
+    public class YearMonth {
+        public int Year { get; init; }
+        public int Month { get; init; }
+
+        public YearMonth(int year, int month) {
+            Year = year;
+            Month = month;
+        }
+        //5.1.2
+        //設定されている西暦が21世紀か判定する　Yearが2001～2100年の間ならture,それ以外ならfalseを返す
+        public bool Is21Century(int year) => year >= 2001 && year <= 2100;
+
+        //5.1.3
+        public YearMonth AddOneMonth() {
+
+        }
+
+
+    }
+}
