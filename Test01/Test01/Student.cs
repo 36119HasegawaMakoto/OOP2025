af93cfd1fd781ee9af7457856e03dbc2a3892398
36119hasegawaMakoto <oibc36119@ojs.ac.jp>
2025-06-17 11:08:22 +0900

    設問1＿Studentクラス完成

---
+++ Test01/Test01/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test01 {
+    public class Student {
+        public string Name { get; init; } //学生の名前
+        public string Subject { get; init; } //科目名
+        public int Score { get; init; } //点数
+    }
+}
