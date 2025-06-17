48842cb833f3d1bc99529fba80bdda0d3942666b
36119hasegawaMakoto <oibc36119@ojs.ac.jp>
2025-06-17 10:13:17 +0900

    8.2.1,2完成

--- chapter08/演習問題/Exercise02/Program.cs
+++ chapter08/演習問題/Exercise02/Program.cs
@@ -8,6 +8,23 @@
             abbrs.Add("IOC", "国際オリンピック委員会");
             abbrs.Add("NPT", "核兵器不拡散条約");
 
+            //8.2.3 Countの呼び出し
+            //上のAddメソッドで、2つのオブジェクトを追加しているので単語数+2が、Countの値になる
+            var count = abbrs.Count;
+            Console.WriteLine(abbrs.Count);
+            Console.WriteLine();
+
+            //8.2.3 Removeの呼び出し
+            if (abbrs.Remove("NPT")) {
+                Console.WriteLine(abbrs.Count);
+            }
+
+            //既に削除してあるのでfalseが返る
+            if (!abbrs.Remove("NPT")) {
+                Console.WriteLine("削除できない");
+            }
+            Console.WriteLine();
+
             // Getメソッドの利用例
             var names = new[] { "WHO", "FIFA", "NPT", };
             foreach (var name in names) {
