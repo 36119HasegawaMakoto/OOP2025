f401a78febe816c74923721012a1de3f8e6d481f
36119hasegawaMakoto <oibc36119@ojs.ac.jp>
2025-06-17 10:33:19 +0900

    8章演習問題＿完成

--- chapter08/演習問題/Exercise02/Program.cs
+++ chapter08/演習問題/Exercise02/Program.cs
@@ -25,6 +25,9 @@
             }
             Console.WriteLine();
 
+            //8.2.4
+            var query = abbrs.GetAll().Where(x => x.Key.Length == 3);
+
             // Getメソッドの利用例
             var names = new[] { "WHO", "FIFA", "NPT", };
             foreach (var name in names) {
