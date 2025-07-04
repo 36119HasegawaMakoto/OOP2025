3ebdab09509b5b5f7c3a47a624701ca3e1c92334
36119hasegawaMakoto <oibc36119@ojs.ac.jp>
2025-07-04 11:39:49 +0900

    イベントハンドラ

--- FormApps/CarReportSYStem/Form1.cs
+++ FormApps/CarReportSYStem/Form1.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel;
+using System.Diagnostics.Metrics;
 
 namespace CarReportSystem {
     public partial class Form1 : Form {
@@ -8,17 +9,17 @@ namespace CarReportSystem {
             InitializeComponent();
             dgvRecord.DataSource = listCarReports;
         }
-
+        //画像開く
         private void btPicOpen_Click(object sender, EventArgs e) {
             if (ofdPicFileOpen.ShowDialog() == DialogResult.OK) {
                 pbPicture.Image = Image.FromFile(ofdPicFileOpen.FileName);
             }
         }
-
+        //画像削除ボタン
         private void btPicDelete_Click(object sender, EventArgs e) {
             pbPicture.Image = null;
         }
-
+        //追加
         private void btRecordAdd_Click(object sender, EventArgs e) {
             var carReport = new CarReport {
                 Date = dtpDate.Value.Date,
@@ -47,11 +48,11 @@ namespace CarReportSystem {
         //記録者の履歴をコンボボックスへ登録（重複なし）
         private void setCbAuthor(string author) {
             //既に登録済みか確認            
-                if (!cbAuthor.Items.Contains(author)) {
-                    //未登録なら登録　とうろくずみならなにもしない
-                    cbAuthor.Items.Add(author);
-                }
+            if (!cbAuthor.Items.Contains(author)) {
+                //未登録なら登録　とうろくずみならなにもしない
+                cbAuthor.Items.Add(author);
             }
+        }
 
         //車名の履歴をコンボボックスへ登録（重複なし）
         private void setcbCarName(string carName) {
@@ -80,9 +81,8 @@ namespace CarReportSystem {
             return CarReport.MakerGroup.その他;
 
         }
-
+        //一覧クリック
         private void dgvRecord_Click(object sender, EventArgs e) {
-
             dtpDate.Value = (DateTime)dgvRecord.CurrentRow.Cells["Date"].Value;
             cbAuthor.Text = (string)dgvRecord.CurrentRow.Cells["Author"].Value;
             setRadioBottonMaker((CarReport.MakerGroup)dgvRecord.CurrentRow.Cells["Maker"].Value);
@@ -118,15 +118,33 @@ namespace CarReportSystem {
         }
         //新規入力のイベントハンドラ
         private void btNewRecord_Click(object sender, EventArgs e) {
-
+            InputItemsAllClear();
         }
         //修正ボタンのイベントハンドラ
         private void btRecordModify_Click(object sender, EventArgs e) {
-
+            if (dgvRecord.CurrentRow == null) {
+            } else {
+                var index = dgvRecord.CurrentRow.Index;
+                listCarReports[index].Date = dtpDate.Value.Date;
+                listCarReports[index].Author = cbAuthor.Text;
+                listCarReports[index].Maker = GetRadioBottonMaker();
+                listCarReports[index].CarName = cbCarName.Text;
+                listCarReports[index].Report = tbReport.Text;
+                listCarReports[index].Picture = pbPicture.Image;
+            }
+            dgvRecord.Refresh();    //データグリッドビューの更新
         }
         //削除ボタンのイベントハンドラ
         private void btRecordDelete_Click(object sender, EventArgs e) {
-
+            if (dgvRecord.CurrentRow == null) {
+            } else {
+                //選択されているインデックスを取得
+                var index = dgvRecord.CurrentRow.Index;
+                listCarReports.RemoveAt(index);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e) {
+            InputItemsAllClear();
         }
     }
 }
