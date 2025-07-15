d93714e37df96249421440a506947d54b615f55b
36119hasegawaMakoto <oibc36119@ojs.ac.jp>
2025-07-15 15:58:32 +0900

    12.3完成

--- chapter12/演習問題/Exercise03/Program.cs
+++ chapter12/演習問題/Exercise03/Program.cs
@@ -1,20 +1,38 @@
-﻿namespace Exercise03 {
+﻿using System.Text.Json;
+using System.IO;
+using System.Xml.Serialization;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using System.Xml;
+
+namespace Exercise03 {
     internal class Program {
         static void Main(string[] args) {
             var employees = Deserialize("employees.json");
             ToXmlFile(employees);
         }
 
-        static Employee[] Deserialize(string filePath) { 
-          
-            
+        static Employee[] Deserialize(string filePath) {
+            var options = new JsonSerializerOptions {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true,
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)
+            };
+            string json = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<Employee[]>(json, options) ?? [];
+
         }
 
-        static void ToXmlFile(Employee[] employees) { 
-
-
+        static void ToXmlFile(Employee[] employees) {            
+            using (var writer = XmlWriter.Create("employee.xml")) {
+                XmlRootAttribute xRoot = new XmlRootAttribute {
+                    ElementName = "Employees"
+                };
+                var serializer = new XmlSerializer(employees.GetType(),xRoot);
+                serializer.Serialize(writer, employees);
+            }
         }
-}
+    }
 
     public record Employee {
         public int Id { get; set; }
