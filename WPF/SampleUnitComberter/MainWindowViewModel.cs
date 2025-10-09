d2e0ced6a36b62329d8c1577f6217ed93743e4bb
36119hasegawaMakoto <oibc36119@ojs.ac.jp>
2025-10-09 13:38:25 +0900

    単位変換アプリ完成Prism変更前

---
+++ WPF/SampleUnitComberter/MainWindowViewModel.cs
@@ -0,0 +1,59 @@
+﻿using SampleUnitConverter;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SampleUnitComberter {
+    class MainWindowViewModel : ViewModel {
+        //フィールド
+        private double metricValue;
+        private double imperialValue;
+
+        //▲でおバレルコマンド
+        public ICommand ImperialUnitToMetric { get; private set; }
+
+        //▼で呼ばれる駒野づ
+        public ICommand MetricUnitToImperial { get; private set; }
+
+        //上のコンボボックスで線たっくされてう裸体
+        public MetricUnit CurrentMetricUnit { get; set; }
+        //下のコンボボックスで選択されている値
+        public ImperialUnit CurrentImperialUnit { get; set; }
+
+        //プロパティ
+        public double MetricValue {
+            get => metricValue;
+            set {
+                this.metricValue = value;
+                this.OnPropertyChanged();
+            }
+
+        }
+        public double ImperialValue {
+            get => imperialValue;
+            set {
+                this.imperialValue = value;
+                this.OnPropertyChanged();
+            }
+
+        }
+        public MainWindowViewModel() {
+            CurrentMetricUnit = MetricUnit.Units.First();
+            CurrentImperialUnit = ImperialUnit.Units.First();
+
+
+            ImperialUnitToMetric = new DelegateCommand(
+                () => MetricValue = 
+                CurrentMetricUnit.FromImperialUnit(CurrentImperialUnit, ImperialValue));
+
+            MetricUnitToImperial = new DelegateCommand(
+                () => MetricValue =
+                CurrentImperialUnit.FromMetricUnit(CurrentMetricUnit, MetricValue));
+        }
+    }
+}
---
+++ WPF/SampleUnitComberter/MetricUnit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleUnitComberter {
+    class MetricUnit : DIstanceUnit {
+        private static List<MetricUnit> units = new List<MetricUnit>{
+            new MetricUnit{Name = "mm",Coefficient = 1, },
+            new MetricUnit{Name = "cm",Coefficient = 10, },
+            new MetricUnit{Name = "m",Coefficient = 10 * 100, },
+            new MetricUnit{Name = "km",Coefficient = 10 * 100 * 1000, },
+
+        };
+        public static ICollection<MetricUnit> Units { get => units; }
+
+        /// <summary>
+        /// ヤードからめーとりゅ
+        /// </summary>
+        /// <param name="unit">変換する単位</param>
+        /// <param name="value">変換する値</param>
+        /// <returns>変換した値</returns>
+        public double FromImperialUnit(ImperialUnit unit, Double value) {
+            return (value * unit.Coefficient) * 25.4 / Coefficient;
+        }
+    }
+}
