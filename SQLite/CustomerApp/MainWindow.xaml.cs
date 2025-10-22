db5f396ef2b74855e3d0e1c0f4efdf11d2ee3d4b
36119hasegawaMakoto <oibc36119@ojs.ac.jp>
2025-10-22 09:34:29 +0900

    顧客管理　UI

---
+++ SQLite/CustomerApp/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomerApp {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void Update_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void PictureButton_Click(object sender, RoutedEventArgs e) {
+
+        }
+    }
+}
