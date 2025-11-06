551378deb26a02c38c319635280cce68ac82c23c
36119hasegawaMakoto <oibc36119@ojs.ac.jp>
2025-11-06 14:48:33 +0900

    LoadingBarの作成

--- chapter14/WebBrowser/MainWindow.xaml.cs
+++ chapter14/WebBrowser/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using Microsoft.Web.WebView2.Core;
+using System.Net.Http;
 using System.Security.Policy;
 using System.Text;
 using System.Windows;
@@ -20,15 +21,35 @@ namespace WebBrowser {
 
         public MainWindow() {
             InitializeComponent();
+            InittializeAsync();
+        }
+        private async void InittializeAsync() {
+            await WebView.EnsureCoreWebView2Async();
 
+            WebView.CoreWebView2.NavigationStarting += CoreWebView2_NavigationString;
+            WebView.CoreWebView2.NavigationCompleted += CoreWebView2_NavigationCompleted;
+        }
+        //読込完了したらプログレスバーを非表示
+        private void CoreWebView2_NavigationCompleted(object? sender, CoreWebView2NavigationCompletedEventArgs e) {
+            LoadingBar.Visibility = Visibility.Collapsed;
+            LoadingBar.IsIndeterminate = false;
+        }
+        //読込開始したらプログレスバーを表示
+        private void CoreWebView2_NavigationString(object? sender, CoreWebView2NavigationStartingEventArgs e) {
+            LoadingBar.Visibility = Visibility.Visible;
+            LoadingBar.IsIndeterminate = true;
         }
 
         private void BackButton_Click(object sender, RoutedEventArgs e) {
-            WebView.GoBack();
+            if (WebView.CanGoBack) {
+                WebView.GoBack();
+            }                        
         }
 
         private void FowardButton_Click(object sender, RoutedEventArgs e) {
-            WebView.GoForward();
+            if (WebView.CanGoForward) {
+                WebView.GoForward();
+            }            
         }
 
         private void GoButton_Click(object sender, RoutedEventArgs e) {
