34e5dd16bccf78531f4f3f20a0a567f155cec489
36119hasegawaMakoto <oibc36119@ojs.ac.jp>
2025-11-12 09:20:07 +0900

    chapter10

---
+++ Exercise01/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            //10-1-1
+            //    var filePath = "Greeting.txt";
+            //    int count = 0;
+            //    using var reader = new StreamReader(filePath);
+            //    while (!reader.EndOfStream) {
+            //        string line = reader.ReadLine();
+            //        if (line.Contains(" Class ")) {
+            //            count++;
+            //        }
+            //        Console.WriteLine(count);
+            //    }
+
+            //10-1-2
+
+            //var filePath = "Greeting.txt";
+            //int count = 0;
+            //var lines = File.ReadAllLines(filePath);
+            //foreach (var item in lines) {
+            //    if (item.Contains(" Class ")) {
+            //        count++;
+            //    }
+            //}
+
+            //10-1-3
+
+            var filePath = "Greeting.txt";
+            int count = 0;
+            var lines = File.ReadLines(filePath);
+            foreach (var item in lines) {
+                if (item.Contains(" Class ")) {
+                    count++;
+                }
+            }
+        }
+    }
+}
---
+++ chapter10/Section02/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Section02 {
+    internal class Program {
+        static void Main(string[] args) {
+            var filePath = "吾輩は猫である.txt";
+            using (var witer = new StreamWriter(filePath)) {
+                witer.WriteLine("いろはにほへと　ちりぬるを");
+                witer.WriteLine("我が世たれぞ　常ならむ");
+                witer.WriteLine("宇井の奥山　今日超えて");
+                witer.WriteLine("あさきゆめみし　酔いもせず");
+            }
+        }
+    }
+}
