b0551768e70136834ac0ae8623d956e48eb7a527
36119hasegawaMakoto <oibc36119@ojs.ac.jp>
2025-11-13 15:55:55 +0900

    strategyパターん

---
+++ chapter15/Strategy/DistanceConverter/DistanceConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistanceConverter {
+    internal class DistanceConverter {
+        public ConverterBase From { get; init; }
+        public ConverterBase To { get; init; }
+
+        //コンストラクタ
+        public DistanceConverter(ConverterBase from, ConverterBase to) {
+            From = from;
+            To = to;
+        }
+        public double Convert(double value) {
+            var meter = From.ToMeter(value);    //メーターへ返還
+            return To.FromMeter(meter);         //メーターからそれっぞれの単位へ変換
+
+        }
+    }
+}
