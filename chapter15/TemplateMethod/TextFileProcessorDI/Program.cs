9fd96c0491ae28a386e12bc9cf3229103873d7b9
36119hasegawaMakoto <oibc36119@ojs.ac.jp>
2025-11-17 15:41:14 +0900

    LineOutputService 完成15.3

--- chapter15/TemplateMethod/TextFileProcessorDI/Program.cs
+++ chapter15/TemplateMethod/TextFileProcessorDI/Program.cs
@@ -1,7 +1,8 @@
 ﻿namespace TextFileProcessorDI {
     internal class Program {
         static void Main(string[] args) {
-            var service = new LineCoumterService();
+            //var service = new LineCoumterService();
+            var service =  new LineOutputService();
             var processor = new TextFileProcessor(service);
             Console.Write("パスの入力");
             processor.Run(Console.ReadLine());
